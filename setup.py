407b46dc8800285f7be8a1b15815e84d14b427da
Sammy Metref <metrefsammy@gmail.com>
2023-11-29 10:07:28 +0100

    Testing requirements file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Needed to actually package something
     packages=['odc_tools'],
     # Needed for dependencies
-    install_requires=required,
+    install_requires=requirements,
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
