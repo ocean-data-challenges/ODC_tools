d4c22bb4d9e8051749d908f2a208d1028c179652
Sammy Metref <metrefsammy@gmail.com>
2023-11-29 10:01:45 +0100

    Testing requirements file

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
 
+from pip.req import parse_requirements
+install_reqs = parse_requirements('requirements.txt', session='hack')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
+#requirements = []
+#with open('requirements.txt', 'r') as f:
+#    lines = [x.strip() for x in f if 0 < len(x.strip())]
+#    requirements = [x for x in lines if x[0].isalpha()]
+
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='odc_tools',
@@ -17,10 +29,6 @@ setup(
     description='Python package of the tools used in the ocean data challenges',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
+
+    install_requires=reqs
 )
-
-
-requirements = []
-with open('requirements.txt', 'r') as f:
-    lines = [x.strip() for x in f if 0 < len(x.strip())]
-    requirements = [x for x in lines if x[0].isalpha()]
